5ea533549d3dd243ddc5c4f5cb6862de3147bb0b
Ayush Kumar Jha <97093341+Neo-Ayush-jha@users.noreply.github.com>
2023-12-26 13:19:53 +0530

    Update RigitSide.tsx

--- app/components/RigitSide.tsx
+++ app/components/RigitSide.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function RigitSide() {
   return (
     <div className='w-full h-full flex flex-col items-center justify-end gap-4 text-textLight'>
-        <Link href="https://ayush91176@gmail.com">
+        <Link href="https://mail.google.com/mail/u/0/?fs=1&to=anishmehta7782@gmail.com&tf=cm">
             <p className='text-sm rotate-90 w-72 tracking-wide text-textGreen'>ayush.kumar.me</p>
         </Link>
         <span className='w-[2px] h-32 bg-textDark inline-flex'></span>
