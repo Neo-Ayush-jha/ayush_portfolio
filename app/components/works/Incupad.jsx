77bd7ea6618e30adaf1861b9e4e605d70cb85421
ayush-incupad <ayush.kumar@incupad.in>
2024-11-10 00:15:48 +0530

    update

--- app/components/works/Incupad.jsx
+++ app/components/works/Incupad.jsx
@@ -8,7 +8,7 @@ function Incupad() {
             <h3 className='flex gap-1 font-medium text-xl font-titleFont'>
                 INCUPAD INDIA<span className='text-textGreen tracking-wide'>Pvt. Ltd. </span>
             </h3>
-            <p className="text-sm mt-1 font-medium text-textDark">DEC 12, 2023 <span className='text-textGreen tracking-wide'>- </span>PRESENT</p>
+            <p className="text-sm mt-1 font-medium text-textDark">DEC 12, 2023 <span className='text-textGreen tracking-wide'>- </span>SEP 12, 2024</p>
             <ul className='mt-6 flex flex-col gap-3'>
                 <li className='flex flex-col lgl:flex-row text-base flex gap-2 text-textDark'>
                     <span className="text-textGreen mt-1 "><TiArrowForward /></span>Frontend Developer
