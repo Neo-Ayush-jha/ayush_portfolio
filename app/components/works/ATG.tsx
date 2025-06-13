d6ccec20b13e99e4a9802c053a01231dff4f9143
Ayush Kumar Jha <97093341+Neo-Ayush-jha@users.noreply.github.com>
2025-06-13 17:34:07 +0530

    Update ATG.tsx

--- app/components/works/ATG.tsx
+++ app/components/works/ATG.tsx
@@ -8,7 +8,7 @@ function ATG() {
             <h3 className='flex gap-1 font-medium text-xl font-titleFont'>
                 ACCROSS THE GLOBE ,<span className='text-textGreen tracking-wide'>BANAO </span>
             </h3>
-            <p className="text-sm mt-1 font-medium text-textDark">AUG, 2023 <span className='text-textGreen tracking-wide'>- </span>MARCH, 2024</p>
+            <p className="text-sm mt-1 font-medium text-textDark">AUGUST, 2023 <span className='text-textGreen tracking-wide'>- </span>MARCH, 2024</p>
             <ul className='mt-6 flex flex-col gap-3'>
                 <li className=' flex flex-col lgl:flex-row gap-2 text-textDark'>
                     <span className="text-textGreen mt-1 "><TiArrowForward /></span> Backend Development and API Integration:
